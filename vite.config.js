c62c68465a08b99435037dc662d246b5a3fb3749
Victor arias <variasca@everis.com>
2023-04-24 07:09:04 -0500

    vitev2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from "url";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 })
